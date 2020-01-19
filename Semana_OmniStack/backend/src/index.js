e3ce0cb96a140d4683c81d086c3b91b02d4a2fbb
Rafael <rafael-rafa2.1@hotmail.com>
2020-01-19 20:19:02 -0300

    Backend
    Backeand final da aplicação.

---
+++ Semana_OmniStack/backend/src/index.js
@@ -0,0 +1,32 @@
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+// Query Params: request.query (filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação  ou alteração  de um registro)
+
+// MongoDB (Não-relacional)
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://userTeste:senhateste@cluster0-yj1i9.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
